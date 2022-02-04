27b090fb5cd7cb0c42b25d208f3749aa3e32b472
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-03 22:01:49 -0800

    debug api GET routes for products, categories, and tags

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,22 +33,22 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-//COME BACK TO PUTS req.body?
-router.post('/', async (req, res) => {
-  // create a new category
-  try {
-    const categoryData = await Category.create(req.body);
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// //COME BACK TO PUTS req.body?
+// router.post('/', async (req, res) => {
+//   // create a new category
+//   try {
+//     const categoryData = await Category.create(req.body);
+//     res.status(200).json(categoryData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+// router.put('/:id', async (req, res) => {
+//   // update a category by its `id` value
+// });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
