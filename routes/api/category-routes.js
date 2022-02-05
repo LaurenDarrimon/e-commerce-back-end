7f8240ba338056e8c16332b3b11dfbce51c04d8c
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-04 22:31:11 -0800

    add POST and PUT routes for api/categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,20 +34,34 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// //COME BACK TO PUTS req.body?
-// router.post('/', async (req, res) => {
-//   // create a new category
-//   try {
-//     const categoryData = await Category.create(req.body);
-//     res.status(200).json(categoryData);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+//COME BACK TO PUTS req.body?
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// router.put('/:id', async (req, res) => {
-//   // update a category by its `id` value
-// });
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No existing category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
