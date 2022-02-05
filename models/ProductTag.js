266ac3a4cad04e1f9ff9bb6d85c34b43d0566dbf
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-04 19:59:09 -0800

    add sequelize syntax for joining table queries with many to many relationships

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,4 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 const sequelize = require('../config/connection');
 
@@ -15,15 +17,17 @@ ProductTag.init(
     product_id:{
       type: DataTypes.INTEGER,
       references: {
-        model: 'product',
+        model: Product,
         key: 'id',
+        unique: false
       },
     }, 
     tag_id:{
       type: DataTypes.INTEGER,
       references: {
-        model: 'tag',
+        model: Tag,
         key: 'id',
+        unique: false
       },
     }, 
     // define columns
--- models/index.js
+++ models/index.js
@@ -7,7 +7,6 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 });
 
 // Categories have many Products
@@ -18,14 +17,20 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { 
-  through: 'ProductTag',
-  onDelete: 'CASCADE',
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags_on_product',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { 
-  through: 'ProductTag', 
-  onDelete: 'CASCADE',
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products_tagged',
 });
   
 
